18a1af3d8d36bbea0c307c32d7cd061e393437be
stavan30 <stavanpatel107@gmail.com>
2024-08-19 18:37:40 -0400

    first commit

--- .history/src/App_20240410143814.js
+++ .history/src/App_20240410143814.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a Demo Project for learning Docker - Stavan Patel
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
--- .history/src/App_20240410144317.js
+++ .history/src/App_20240410144317.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a Demo Project for learning Docker - Stavan Patel
+          This is a Demo Project for learning Docker
         </p>
         <a
           className="App-link"
